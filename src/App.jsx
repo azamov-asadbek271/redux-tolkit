9e6f2ffa6aa79c6554db2beb9be1848dafd6a5c7
azamov-asadbek271 <azamjonovasa45@gmail.com>
2024-04-28 20:49:39 +0300

    qqqqq

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
 import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import Navbar from './componentss/Navbar'
 import ProductsList from './componentss/ProductsList'
+import { calculateTotal } from './features/products/ProducrsSlice'
+import { useSelector } from "react-redux";
 
 function App() {
+  const { price } = useSelector((state) => state);
+  
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(calculateTotal());
+  },[])
   return (
     <div className='max-w-6xl mx-auto '>
       <Navbar/>
+      <h2 className='p-3 mb-10 text-2xl'>{price} $</h2>
       <ProductsList/>
     </div>
   )
